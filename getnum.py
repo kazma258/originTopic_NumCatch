5eff719b69ca5801da83da9a43c2fc6de8876815
kazma258 <blues6163853@gmail.com>
2021-05-19 16:32:10 +0800

    2021/5/19 debuging

--- getnum.py
+++ getnum.py
@@ -1,39 +1,71 @@
+#3辨識圖片的數字
 import cv2
 from numpy import imag
 import pytesseract
 import os
 import getphoto
-import tesserocr
-from PIL import Image
 
 def numget(img):
     pytesseract.pytesseract.tesseract_cmd = r"D:/pytesseract/tesseract.exe"
     #img.show()
     result = ""
-    output = pytesseract.image_to_string(img, lang="eng", config="--psm 10")
+    output = pytesseract.image_to_string(img, lang="eng", config="--psm 8")
     for i in range(len(output)):
-        # if output[i].isdigit():
-        #     result += output[i]
-        return output
+        if output[i].isdigit():
+            result += output[i]
+    return result
 
 def mask(imgpath):
     img = cv2.imread(imgpath)
     # img = getphoto.greenmask(img)
     return img
 
-if __name__ == "__main__":
+def write_numfile():
     folder = r"./Numdata/"
     filelist = os.listdir(folder)
-    # for file in filelist:
-    #     img = getphoto.imgq(mask(folder+file))
-    #     cv2.imshow(file, img)
-    #     num = numget(img)
-    #     print('{} \nThe result is: {}'.format(folder+file, num))
-
+    f = open(r'./result.txt', 'w')
     for file in filelist:
-        image = Image.open(folder+file)
-        print(file)
-        print(tesserocr.image_to_text(image)+'\n')
+        img = getphoto.imgq(mask(folder+file))
+        # cv2.imshow(file, img)
+        num = numget(img)
+        # print('{} \nThe result is: {}'.format(folder+file, num))
+        if('.jpg' in file):
+            file = file[:-4]
+            file = file.replace(',', ' ')
+        # print(file)
+        f.write('{} {}\n'.format(file, num))
+    f.close()
+
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
+def lenl(var):
+    return{
+        0 : map[i].x,
+        1 : map[i].y,
+        2 : map[i].num
+    }.get(var, 'error')
+
+class num_Map:
+    def __init__(self, x, y, num):
+        self.x = x
+        self.y = y
+        self.num = num
+
+if __name__ == "__main__":
+    write_numfile()
+    f = open(r'./result.txt')
+    data = f.read().splitlines()
+    for i in range(len(data)):
+        data[i] = data[i].split(' ')
+
+    map = [num_Map(0,0,0)] * 11
+    for i in range(len(data)):
+        for n in range(len(data[i])):
+            lenl[n] = data[i][n]
+            # if(n is 0):
+            #     map[i].x = data[i][n]
+            # elif(n is 1): map[i].y = data[i][n]
+        print('{} {} {}'.format(map[i].x, map[i].y, map[i].num))
     
-    # cv2.waitKey()
-    # cv2.destroyAllWindows()
+    os.system('pause')
